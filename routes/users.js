f61a6d8966bbe0de0187f9de0fe844fec8b49760
Mahreen <mahreenabbad@gmail.com>
2024-08-13 17:37:00 +0500

    Add Project

---
+++ routes/users.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  contact: String,
+  bio: String,
+  stories: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "story" 
+    }
+  ],
+  saved: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post" 
+    }
+  ],
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "post" 
+  }],
+  followers: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user" 
+    } 
+  ],
+  following: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user" 
+    }
+  ]
+})
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("user", userSchema);
